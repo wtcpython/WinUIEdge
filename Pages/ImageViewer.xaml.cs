9f2714efaad9f9d6241c7c7518f3aa7164ecb87e
wtcpython <1762993226@qq.com>
2024-03-06 22:13:45 +0800

    图片裁切因为问题过多暂时去除，代码优化

--- Pages/ImageViewer.xaml.cs
+++ Pages/ImageViewer.xaml.cs
@@ -5,6 +5,7 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Windows.Storage;
 using Windows.Storage.FileProperties;
@@ -19,6 +20,8 @@ namespace Edge
         public string fileName;
 
         public int Angle = 0;
+
+        public Dictionary<string, string> dict = [];
 
         public ImageViewer(string filepath)
         {
@@ -40,36 +43,41 @@ namespace Edge
             ItemImage.Source = new BitmapImage(new Uri(filePath));
 
             imageNameBlock.Text = filePath;
-            imageName.Text = fileName = file.DisplayName;
-            imageType.Text = file.DisplayType;
-            imagePixel.Text = $"{properties.Width} x {properties.Height}";
-            imageSize.Text = Converters.ToFileSizeString(ImageFileSize);
+            dict["文件名称"] = file.DisplayName;
+            dict["文件类型"] = file.DisplayType;
+            dict["像素大小"] = $"{properties.Width} x {properties.Height}";
+            dict["文件大小"] = Converters.ToFileSizeString(ImageFileSize);
+            //imageName.Text = fileName = file.DisplayName;
+            //imageType.Text = file.DisplayType;
+            //imagePixel.Text = $"{properties.Width} x {properties.Height}";
+            //imageSize.Text = Converters.ToFileSizeString(ImageFileSize);
+            view.ItemsSource = dict;
 
             imageRating.Value = properties.Rating;
         }
 
-        private async void FileNameChanged(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
-        {
-            if (e.Key == Windows.System.VirtualKey.Enter)
-            {
-                if (fileName != imageName.Text)
-                {
-                    FileInfo fileInfo = new(filePath);
-                    string fileExt = fileInfo.Extension;
-                    bool changed = await Dialog.ShowMsgDialog(
-                        App.GetWindowForElement(this).Content.XamlRoot,
-                        "文件名称变更确认", $"是否要将名称从 {fileName + fileExt} 更改为 {imageName.Text + fileExt} ?", "取消", "确定");
-                    if (changed)
-                    {
-                        fileInfo.MoveTo(fileInfo.Directory + "\\" + imageName.Text + fileExt);
-                    }
-                    else
-                    {
-                        imageName.Text = fileName;
-                    }
-                }
-            }
-        }
+        //private async void FileNameChanged(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+        //{
+        //    if (e.Key == Windows.System.VirtualKey.Enter)
+        //    {
+        //        if (fileName != imageName.Text)
+        //        {
+        //            FileInfo fileInfo = new(filePath);
+        //            string fileExt = fileInfo.Extension;
+        //            bool changed = await Dialog.ShowMsgDialog(
+        //                App.GetWindowForElement(this).Content.XamlRoot,
+        //                "文件名称变更确认", $"是否要将名称从 {fileName + fileExt} 更改为 {imageName.Text + fileExt} ?", "取消", "确定");
+        //            if (changed)
+        //            {
+        //                fileInfo.MoveTo(fileInfo.Directory + "\\" + imageName.Text + fileExt);
+        //            }
+        //            else
+        //            {
+        //                imageName.Text = fileName;
+        //            }
+        //        }
+        //    }
+        //}
 
         private async void ImageDeleteRequest(object sender, RoutedEventArgs e)
         {
@@ -96,10 +104,21 @@ namespace Edge
             ItemImage.RenderTransform = rotateTransform;
         }
 
-        private void ImageCropRequest(object sender, RoutedEventArgs e)
+        private void ScrollViewer_DoubleTapped(object sender, Microsoft.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
         {
-            MainWindow mainWindow = App.GetWindowForElement(this);
-            mainWindow.AddNewTab(new ImageCropPage(filePath));
+            var scrollViewer = sender as ScrollViewer;
+            var doubleTapPoint = e.GetPosition(scrollViewer);
+
+            if (scrollViewer.ZoomFactor != 1)
+            {
+                scrollViewer.ZoomToFactor(1);
+            }
+            else
+            {
+                scrollViewer.ZoomToFactor(2);
+                scrollViewer.ScrollToHorizontalOffset(doubleTapPoint.X);
+                scrollViewer.ScrollToVerticalOffset(doubleTapPoint.Y);
+            }
         }
     }
 }
