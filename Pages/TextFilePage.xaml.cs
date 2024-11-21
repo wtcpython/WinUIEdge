f62f8a3c9e77bfe4c5842087a66f3b24e1e852f9
wtcpython <1762993226@qq.com>
2024-11-21 19:50:01 +0800

    将 Binding 改为 x:Bind

--- Pages/TextFilePage.xaml.cs
+++ Pages/TextFilePage.xaml.cs
@@ -3,7 +3,6 @@ using Microsoft.UI;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Documents;
 using Microsoft.UI.Xaml.Media;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -29,22 +28,19 @@ namespace Edge
         {
             this.InitializeComponent();
 
+            file = filepath;
             string ext = Path.GetExtension(filepath);
             typeName = Info.LanguageDict.RootElement.GetProperty(ext).ToString();
 
             // 加载编码列表
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             encodeList = Encoding.GetEncodings();
-            fileControl.EncodingItems = encodeList;
-            fileControl.SelectionChanged += EncodeTypeChanged;
-
-            // 加载文件信息
-            file = filepath;
-            AnalyzeFile(file, Encoding.Default);
+            EncodingComboBox.ItemsSource = encodeList;
+            EncodingComboBox.SelectedItem = encodeList.Last();
 
             // 初始化UI 数据
-            fileControl.FullPath = file;
-            fileControl.TypeName = typeName;
+            FullPath.Text = file;
+            TypeName.Text = typeName;
 
             block.FontFamily = new FontFamily(DefaultFontFamily);
             block.FontSize = DefaultFontSize;
@@ -72,26 +68,21 @@ namespace Edge
 
         private void EncodeTypeChanged(object sender, SelectionChangedEventArgs e)
         {
-            AnalyzeFile(file, encodeList[(sender as FileControl).EncodingSelectedIndex].GetEncoding());
-        }
-
-        private void AnalyzeFile(string filePath, Encoding encoding)
-        {
+            Encoding encoding = encodeList[(sender as ComboBox).SelectedIndex].GetEncoding();
             // 分析文件信息，包含文本内容，文本长度，文本行尾序列和文本编码格式
-            StreamReader reader = null;
+            StreamReader reader;
             if (encoding == Encoding.Default)
             {
-                reader = new StreamReader(filePath, Encoding.Default, true);
+                reader = new StreamReader(file, Encoding.Default, true);
             }
             else
             {
-                reader = new StreamReader(filePath, encoding);
+                reader = new StreamReader(file, encoding);
             }
 
             string content = reader.ReadToEnd();
-            fileControl.LengthInfo = $"共 {content.Length} 个字符";
-            fileControl.EOF = GetEOF(content);
-            fileControl.EncodingSelectedIndex = Array.FindIndex(encodeList, x => x.Name == reader.CurrentEncoding.BodyName);
+            LengthInfo.Text = $"共 {content.Length} 个字符";
+            EOF.Text = GetEOF(content);
 
             SetHighlightContent(content);
         }
