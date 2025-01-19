085595a2121bbd4ebb86f176942ae37dd18df540
wtcpython <1762993226@qq.com>
2025-01-20 01:47:57 +0800

    收藏夹显示不全时添加滚动条

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,7 +2,6 @@ using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.Web.WebView2.Core;
 using System;
 using System.ComponentModel;
 using Windows.Foundation;
