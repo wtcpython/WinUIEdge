0af5f6df52896fa0161cfc212623c24a542aa003
wtcpython <1762993226@qq.com>
2025-03-20 12:45:16 +0800

    添加 Github Release 发行说明显示

---
+++ Settings/ReleaseItem.xaml.cs
@@ -0,0 +1,52 @@
+using CommunityToolkit.Labs.WinUI.MarkdownTextBlock;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace Edge
+{
+    public class GitHubRelease
+    {
+        public string name { get; set; }
+        public string body { get; set; }
+    }
+
+    public sealed partial class ReleaseItem : Page
+    {
+        public ReleaseItem()
+        {
+            this.InitializeComponent();
+            markdown.Config = new MarkdownConfig();
+            SetText();
+        }
+
+        public async void SetText()
+        {
+            List<GitHubRelease> releases = await GetReleasesAsync();
+            string contents = string.Empty;
+            releases.ForEach(item =>
+            {
+                string content = "# " + item.name + "\n" + item.body + "\n";
+                contents += content;
+            });
+            markdown.Text = contents;
+        }
+
+        public static async Task<List<GitHubRelease>> GetReleasesAsync()
+        {
+            HttpClient client = new();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("WinUIEdge/1.0");
+            var response = await client.GetAsync("https://api.github.com/repos/wtcpython/WinUIEdge/releases");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<GitHubRelease>>(json);
+            }
+            return null;
+        }
+    }
+}
