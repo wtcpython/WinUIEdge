79e9ff22cd672963e8ff7e3e3e6a721cf37190cc
XiaofengdiZhu <523084467@qq.com>
2025-05-04 02:10:27 +0800

    完善Edge扩展商店注入脚本

--- Data/InjectExtensionsStoreScript.js
+++ Data/InjectExtensionsStoreScript.js
@@ -1,18 +1,63 @@
 ﻿console.log("WinUIEdge is injecting");
-const ovserver = new MutationObserver(function(mutationsList) {
+let styleAdded = false;
+const observer = new MutationObserver(function(mutationsList) {
     for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
+        if (mutation.type === "childList") {
             const addedNodes = mutation.addedNodes;
-            for (let i = 0; i < addedNodes.length; i++) {
-                const addedNode = addedNodes[i];
-                if (addedNode.role === 'main') {
-                    const originalInstallButton = document.querySelector("button[id^=installButton-]");
-                    const installButtonParent = originalInstallButton?.parentElement?.parentElement;
-                    if (!installButtonParent) {
-                        return;
-                    }
-                    const styleElement = document.createElement("style");
-                    styleElement.innerText = `#injectInstallButton {
+            for (let node of addedNodes) {
+                findAndReplaceInstallButtons(node);
+            }
+        }
+    }
+});
+
+const rootElement = document.getElementById("root");
+if(rootElement) {
+    findAndReplaceInstallButtons(rootElement);
+    observer.observe(document.getElementById("root"), {childList: true, subtree: true, attributes: true, characterData: true});
+}
+
+function findAndReplaceInstallButtons(element) {
+    const originalInstallButtons = findInstallButtons(element);
+    if (originalInstallButtons.length === 0) {
+        return;
+    }
+    for (let originalInstallButton of originalInstallButtons) {
+        if (!styleAdded) {
+            addStyle();
+            styleAdded = true;
+        }
+        replaceButton(originalInstallButton);
+    }
+}
+
+function findInstallButtons(element) {
+    const result = [];
+    if (element.children && element.children.length > 0) {
+        for (let child of element.children) {
+            if (child.id && child.id.startsWith("installButton-") && !child.classList.contains("replacedInstallButton")) {
+                result.push(child);
+                continue;
+            }
+            result.push(...findInstallButtons(child));
+        }
+    }
+    return result;
+}
+
+function replaceButton(originalInstallButton){
+    const installAnchorElement = document.createElement("a");
+    installAnchorElement.href = `https://edge.microsoft.com/extensionwebstorebase/v1/crx?response=redirect&x=id%3D${originalInstallButton.id.slice(14)}%26installsource%3Dondemand`;
+    installAnchorElement.innerText = "获取";
+    installAnchorElement.download;
+    originalInstallButton.classList.add("replacedInstallButton");
+    originalInstallButton.removeChild(originalInstallButton.firstElementChild);
+    originalInstallButton.appendChild(installAnchorElement);
+}
+
+function addStyle(){
+    const styleElement = document.createElement("style");
+    styleElement.innerText = `.injectInstallButton {
     background-color: var(--colorBrandBackground);
     border: var(--strokeWidthThin) solid var(--colorNeutralStroke1);
     border-radius: 4px;
@@ -36,20 +81,23 @@ const ovserver = new MutationObserver(function(mutationsList) {
         background-color: var(--colorBrandBackgroundHover);
     }
 }
-    `;
-                    document.head.appendChild(styleElement);
-                    const installAnchorElement = document.createElement("a");
-                    installAnchorElement.href = `https://edge.microsoft.com/extensionwebstorebase/v1/crx?response=redirect&x=id%3D${originalInstallButton.id.slice(14)}%26installsource%3Dondemand`;
-                    installAnchorElement.innerText = "安装到 WinUIEdge";
-                    installAnchorElement.download;
-                    const installButtonElement = document.createElement("button");
-                    installButtonElement.id = "injectInstallButton";
-                    installButtonElement.appendChild(installAnchorElement);
-                    installButtonParent.appendChild(installButtonElement);
-                    ovserver.disconnect();
-                }
-            }
-        }
+.replacedInstallButton {
+    background-color: var(--colorBrandBackground);
+    cursor: pointer;
+    padding: 0;
+    &>a {
+        color: var(--colorNeutralForegroundOnBrand);
+        display: block;
+        padding: 5px var(--spacingHorizontalM);
+        text-decoration: none;
     }
-});
-ovserver.observe(document.getElementById("root"), { childList: true, subtree: true });
+    &:hover {
+        background-color: var(--colorBrandBackgroundHover);
+        cursor: pointer;
+    }
+}
+.incompatible {
+    display: none;
+}`;
+    document.head.appendChild(styleElement);
+}
